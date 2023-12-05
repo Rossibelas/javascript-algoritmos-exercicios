76daa5394329430d3eb9911162aca8dde999b48b
Rossibelas <guilhermerossi103@gmail.com>
2023-12-04 22:26:32 -0300

    Botões 04 12 23

---
+++ Botões 04 12 23/Banda/app.js
@@ -0,0 +1,11 @@
+function Straight(){
+    document.getElementById("imagem").src="Straight outta compton.webp"
+    document.getElementById("mensagem").innerText = "Grupo rapper que escandalizou os EUA, leva sua história ao cinema"
+    document.getElementById("frase").innerText = "Quase três décadas depois de causar entre comunidades norte-americanas, o filme baseado na história do grupo N.W.A estreia nesta sexta-feira nos Estados Unidos e mostra que suas denúncias continuam mais vivas do que nunca"
+    
+}
+function Nirvana(){
+        document.getElementById("imagem").src="Straight outta compton.webp"
+        document.getElementById("mensagem").innerText = "Grupo rapper que escandalizou os EUA, leva sua história ao cinema"
+        document.getElementById("frase").innerText = "Quase três décadas depois de causar entre comunidades norte-americanas, o filme baseado na história do grupo N.W.A estreia nesta sexta-feira nos Estados Unidos e mostra que suas denúncias continuam mais vivas do que nunca"
+}
---
+++ Botões 04 12 23/Gedai/app.js
@@ -0,0 +1,14 @@
+function jedi(){
+    document.getElementById("mensagem").innerText = "Venha para o lado da Luz"
+    document.getElementById("frase").innerText = "Guerras Não Fazem Niguém Maior"
+    document.getElementById("imagem").src="jedi.gif"
+    document.body.style="background: #607479"
+}
+
+function sith(){
+    document.getElementById("mensagem").innerText = "Venha para o lado sombrio"
+    document.getElementById("frase").innerText = "A paz é uma mentira, só a emoção importa"
+    document.getElementById("imagem").src="sith.gif"
+    document.body.style="background: #4f5055"
+
+}
